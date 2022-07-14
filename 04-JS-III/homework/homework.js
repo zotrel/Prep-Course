4d1520bf4c4a79c2a49e26f81d330649f820f035
Flor March <106163869+zotrel@users.noreply.github.com>
2022-07-14 14:31:37 -0300

    arreglo04

--- 04-JS-III/homework/homework.js
+++ 04-JS-III/homework/homework.js
@@ -130,7 +130,14 @@ function mayorACien(array) {
   //La función recibe un array con enteros entre 0 y 200. Recorrer el array y guardar en un nuevo array sólo los
   //valores mayores a 100 (no incluye el 100). Finalmente devolver el nuevo array.
   // Tu código:
+  var nuevoArray=[];
+  for (var i = 0; i < array.length; i++) {
+    if (array[i] > 100) { 
+    nuevoArray.push(array[i])
+  }}
+  return nuevoArray
 }
+
 
 
 function breakStatement(numero) {
